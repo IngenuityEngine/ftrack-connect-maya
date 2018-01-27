a3404fd700d2855c79c618540de5b0e0c7aadedc
Nellie Robinson <nelliearobinson@gmail.com>
2018-01-26 16:35:03 -0800

    get username from ftrack api user, not from getpass

--- resource/hook/ftrack_connect_maya_hook.py
+++ resource/hook/ftrack_connect_maya_hook.py
@@ -63,7 +63,7 @@ class LaunchApplicationAction(object):
         '''Register discover actions on logged in user.'''
         self.session.event_hub.subscribe(
             'topic=ftrack.action.discover and source.user.username={0}'.format(
-                getpass.getuser()
+                self.session.api_user
             ),
             self.discover,
             priority=10
@@ -72,7 +72,7 @@ class LaunchApplicationAction(object):
         self.session.event_hub.subscribe(
             'topic=ftrack.action.launch and source.user.username={0} '
             'and data.actionIdentifier={1}'.format(
-                getpass.getuser(), self.identifier
+                self.session.api_user, self.identifier
             ),
             self.launch
         )
